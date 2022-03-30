0d6b15d38f99bfef5b20fa5511c8a5e1d737937f
Marckms26 <74006142+Marckms26@users.noreply.github.com>
2022-03-30 00:00:34 -0300

    Classe do usuario

--- src/main/java/com/environment/aplication/v1/CreateUser.java
+++ src/main/java/com/environment/aplication/v1/CreateUser.java
@@ -2,15 +2,28 @@ package com.environment.aplication.v1;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.FieldNamingPolicy;
+
+import com.environment.domain.User;
 
 /**
  * Cria um novo usuario
  */
 @RestController
-public class CreateUser {
-    
+public class CreateUser {    
     @PostMapping("/api/v1/user")
-    public String index() {
+    public String index(@RequestBody String body) {
+
+        Gson gson = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES).create();
+        User user = gson.fromJson(body, User.class);
+
+        System.out.println("req: "+user);
+
+        user.extract();
+
         return "CREATE USER";
     }
     
--- src/main/java/com/environment/domain/User.java
+++ src/main/java/com/environment/domain/User.java
@@ -1,8 +1,54 @@
 package com.environment.domain;
 
+/**
+ * Classe responsável à definir e gerenciar o usuário
+ * 
+ */
 public class User {
 
     String uuid;
-    String name;
+    String firstName;
+    String lastName;
+    String fullName;
+    String phone;
+    String email;
+    String password;
+    String sault;
+    String uidDevice;
+
+    public User(String uuid, String firstName, String lastName, String phone, String email, String password, String sault) {
+        this.uuid = uuid;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phone = phone;
+        this.email = email;
+        this.password = password;
+        this.sault = sault;
+    }
+
+    public Boolean validate() {
+        return true;
+    }
+
+    public String save() {
+        return "";
+    }
+
+    public String update() {
+        return "";
+    }
+
+    public String list() {
+        return "";
+    }
+
+    public String delete() {
+        return "";
+    }
+
+    public String extract() {
+        System.out.println("firstName: "+this.lastName);
+        return "";
+    }
 
 }
